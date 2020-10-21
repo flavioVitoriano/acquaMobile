23227c115f5dfdb3ada3d3b291de390f0e7e0f34
CarlosSTS <carlossts826@gmail.com>
2020-10-21 19:27:29 -0300

    importando intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import 'react-native-gesture-handler';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar, View} from 'react-native';
